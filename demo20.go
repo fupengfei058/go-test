90d820e22b02b6c05b9bb5c60ba2d5660523377d
fupengfei <fupengfei@yaochufa.com>
2017-07-19 10:34:26 +0800

    go-test

---
+++ demo20.go
@@ -0,0 +1,16 @@
+// demo20
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	messages := make(chan string, 2)
+
+	messages <- "buffered"
+	messages <- "channel"
+
+	fmt.Println(<-messages)
+	fmt.Println(<-messages)
+}
